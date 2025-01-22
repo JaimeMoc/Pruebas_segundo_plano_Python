f1be70d63917ee7e96b43413041e316a0a5d2756
Jaime Alberto Suarez Moctezuma <Suarezjaime2712@gmail.com>
2025-01-22 14:05:56 -0600

    Cambios en los threads.

--- app1/tasks.py
+++ app1/tasks.py
@@ -2,8 +2,8 @@ import time
 from threading import Thread
 
 def asyn_send_mail(to):
-    task= Thread(target=send_mail, args=(to,))
-    task.start()
+    task= Thread(target=send_mail, args=(to,)) #Creación de un segundo Hilo.
+    task.start() 
 
 def send_mail(to):
     time.sleep(2)
