0b893a0d074a1cfc05b24785884b62b4e5811482
Jaime Alberto Suarez Moctezuma <Suarezjaime2712@gmail.com>
2025-01-22 13:04:23 -0600

    Agregamos la funcionalidad que nos permitira simular el envio del correo electronico. Lanzando un mensaje como "El correo fue enviado exitosamente".

---
+++ app1/tasks.py
@@ -0,0 +1,5 @@
+import time
+
+def send_mail(to):
+    time.sleep(2)
+    print(">>> Hemos enviado el correo.")
--- app1/views.py
+++ app1/views.py
@@ -1,8 +1,17 @@
 from django.shortcuts import render
+from app1.tasks import send_mail
 
 #Creación de nuestro index.
 def index(request):
-    return render(request, 'index.html', {
-        'mail_sent': False
-    })
+    
+    mail_sent = False
+    
+    if  request.method == 'POST':
+        email = request.POST.get('email')   
+        send_mail(email)        
 
+        mail_sent = True
+    
+    return render(request, 'index.html', {
+        'mail_sent': mail_sent
+    })
