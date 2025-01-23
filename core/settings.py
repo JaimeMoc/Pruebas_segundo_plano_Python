665318ad5623442668829b87a5d92e44eec4acdc
Jaime Alberto Suarez Moctezuma <Suarezjaime2712@gmail.com>
2025-01-22 18:55:02 -0600

    Cambios en celery

--- core/settings.py
+++ core/settings.py
@@ -125,3 +125,4 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # Configuración de redis.
 CELERY_BROKER_URL = "redis://localhost:6379"
 CELERY_RESULT_BACKEND = "redis://localhost:6379"
+CELERY_BROKER_CONNECTION_RETRY_ON_STARTUP = True
